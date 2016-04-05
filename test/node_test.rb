f53981e1de08c3d652ace4112d9edd19d760f6f7
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-04-05 14:14:14 -0600

    Making classes

---
+++ test/node_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/node'
+
+class NodeTest < MiniTest::Unit::TestCase
+
+  
+end
---
+++ test/trie_test.rb
@@ -0,0 +1,3 @@
+require "./lib/tasty_pizza"
+#tasty_pizza.rb, tasty_pizza_test.rb
+#lib: contains tasty_pizza.rb, test: contains tasty_piza_test.rb
